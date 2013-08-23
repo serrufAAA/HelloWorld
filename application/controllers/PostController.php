b82cb8279c5fe4af32bff3b4a9d72c9003bd05bc
serrufAAA <serruf@mail.ru>
2013-08-23 22:38:23 +0600

    siparete Post & Comment

--- application/controllers/PostController.php
+++ application/controllers/PostController.php
@@ -1,15 +1,17 @@
 <?php
-require_once "application/models/PostModel.php";
 class PostController extends Controller
 {
-	function __construct()
+	/*function __construct()
     {
         $this->view = new View();
         $this->model = new PostModel();
-    }
+    }*/
 
-    function actionAdd(){		
-        $this->view->generate('PostAddView.php');
-        $this->model->Add();
+    function actionAdd(){
+    $this->view = new View();
+    $this->model = new PostModel();
+    //$this->view->render('application/views/PostAddView.php');
+    $this->view->generate('PostAddView.php');
+    $this->model->Add();
     }
 }
